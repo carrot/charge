41f80989dfb464b51513ced2ef10b06854d33f4f
Kyle MacDonald <kyle@carrotcreative.com>
2014-05-14 17:59:51 -0400

    add charge.json example. still wip

---
+++ examples/config_file/server.js
@@ -0,0 +1,9 @@
+require('coffee-script/register')
+
+var charge = require('../..'),
+    http = require('http');
+
+// create a new charge app
+// charge automatically uses the config options in (__dirname + 'charge.json')
+var app = charge(__dirname);
+http.createServer(app).listen(1111);
